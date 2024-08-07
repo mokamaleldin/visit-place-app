c38c0c232613d518f514cacfd201b421c60ad078
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-08-07 19:43:53 +0300

    chore: Add geolocation support for sorting available places by distance

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { useRef, useState } from 'react';
-
+import { useRef, useState, useEffect } from 'react';
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [availablePlases, setAvailablePlaces] = useState([]);
+
+
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -39,15 +53,15 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal ref={ modal }>
         <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
+          onCancel={ handleStopRemovePlace }
+          onConfirm={ handleRemovePlace }
         />
       </Modal>
 
       <header>
-        <img src={logoImg} alt="Stylized globe" />
+        <img src={ logoImg } alt="Stylized globe" />
         <h1>PlacePicker</h1>
         <p>
           Create your personal collection of places you would like to visit or
@@ -57,14 +71,15 @@ function App() {
       <main>
         <Places
           title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
-          places={pickedPlaces}
-          onSelectPlace={handleStartRemovePlace}
+          fallbackText={ 'Select the places you would like to visit below.' }
+          places={ pickedPlaces }
+          onSelectPlace={ handleStartRemovePlace }
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
-          onSelectPlace={handleSelectPlace}
+          fallbackText={ 'Loading available places...' }
+          places={ availablePlases }
+          onSelectPlace={ handleSelectPlace }
         />
       </main>
     </>
